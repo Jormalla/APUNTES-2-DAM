60885c7118c35035c5ec352cdfb663c69e83d11a
Jormalla <jormalla@gmail.com>
2026-09-23 17:53:04 +0200

    wip recorrido de vista de directorios

--- AccesoDatos/UD1/R1/PrepararCarpetas.java
+++ AccesoDatos/UD1/R1/PrepararCarpetas.java
@@ -1,6 +1,7 @@
 package AccesoDatos.UD1.R1;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -29,6 +30,19 @@ public class PrepararCarpetas {
             System.err.println("No se pudo acceder al archivo: "
                     + e.getMessage());
         }
+
+        // Mostrar los bytes de cada archivo y si existe
+        // Primero en directorio datos
+        try (DirectoryStream<Path> elementosEnDatos = Files.newDirectoryStream(datos)) {
+            for (Path elemento : elementosEnDatos) {
+                if (Files.isRegularFile(elemento)) {
+                    System.out.println(elemento.getFileName());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 
 }
